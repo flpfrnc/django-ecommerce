b8527d9e17f1745e2b489722312b6f2bd3be657b
Felipe <felipevictor67@hotmail.com>
2021-01-11 01:47:57 -0300

    finished structuring cart and checkout

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -37,6 +37,18 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
@@ -44,6 +56,10 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAdress(models.Model):
